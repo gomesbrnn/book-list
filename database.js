333ef82bff0ef8a45f66d77a40e1b51c172ccb51
gomesbrnn <brngomesds@gmail.com>
2021-07-08 20:57:40 -0300

    creating book list database

---
+++ database.js
@@ -0,0 +1,36 @@
+const livros = [
+
+    {
+        id: 1,
+        nome: 'Do mil ao milhão',
+        autor: 'Thiago Nigro',
+        categoria: 'Finanças',
+        paginas: 192,
+        recomenda: true,
+        leu: false
+
+    },
+
+    {
+        id: 2,
+        nome: 'O menino de pijama listrado',
+        autor: 'John Boyne',
+        categoria: 'Documental',
+        paginas: 192,
+        recomenda: true,
+        leu: true
+
+    },
+
+    {
+        id: 3,
+        nome: 'A sutil arte de ligar o f*da-se',
+        autor: 'Mark Manson',
+        categoria: 'Desenvolvimento Pessoal',
+        paginas: 224,
+        recomenda: true,
+        leu: false
+
+    }
+
+]
